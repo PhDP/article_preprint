7617d033f1edcece0a85610e40756eb6303f7765
Karthik Ram <karthik.ram@gmail.com>
2012-12-19 09:40:07 -0800

    changed color scheme to make biology pop out

--- arxiv_figure/figure.R
+++ arxiv_figure/figure.R
@@ -15,8 +15,11 @@ foo3 <- melt(foo[, c("year",names(foo)[!grepl("percent", names(foo))])], id.vars
 # Remove year.1
 foo3 <- foo3[foo3$variable!="year.1",]
 
-foo3$shape <- as.factor(ifelse(grepl("Biology", foo3$variable), 2, 1))
+foo3$variable <-  factor(foo3$variable, levels = c("Astronomy and Astrophysics", "Condensed Matter", "Mathematics", "High-energy Physics", "Other Physics", "Computer Science", "Statistics", "Finance", "Biology"))
 
-figure_2 <- ggplot(foo3[complete.cases(foo3),], aes(year, value, group = variable,color = variable)) + geom_line() + geom_line(aes(linetype = shape), size = 1.3, , show_guide = FALSE) + expand_limits(0,0) + labs(y="Percent of submissions", x="Year") + ggtitle("arXiv submissions by discipline") + guides(colour = guide_legend(title = "Disciplines", title.hjust = 0.5, keywidth = 1)) + scale_color_manual(values = rev(c(brewer.pal(9, "Spectral"))),  name = "Discipline") + theme_gray()
+foo3$shape <- as.factor(ifelse(grepl("Biology", foo3$variable), 2, 1))
+pcolor <- rev(c(brewer.pal(9, "YlGnBu")))
+pcolor[9] <- "#d53d4e"
+figure_2 <- ggplot(foo3[complete.cases(foo3),], aes(year, value, group = variable,color = variable)) + geom_line() + geom_line(aes(linetype = shape), size = 1.3, , show_guide = FALSE) + expand_limits(0,0) + labs(y="Percent of submissions", x="Year") + ggtitle("arXiv submissions by discipline") + guides(colour = guide_legend(title = "Disciplines", title.hjust = 0.5, keywidth = 1)) + scale_color_manual(values = pcolor,  name = "Discipline") + theme_gray()
 
 ggsave(figure_2, file = "figure_2.png", width = 6, height = 4)
