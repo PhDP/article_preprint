6fee7c54e9abc5e6c0efeb5b3a39dfd4b3442dac
Karthik Ram <karthik.ram@gmail.com>
2013-01-26 12:17:38 -0800

    switched figure to eps as requested by PhDP

--- arxiv_figure/figure.R
+++ arxiv_figure/figure.R
@@ -2,7 +2,7 @@ library(plyr)
 library(reshape2)
 library(ggplot2)
 library(RColorBrewer)
-# setwd('~/Desktop/Outbox/article_preprint/arxiv_figure/')
+setwd('~/Desktop/article_preprint/arxiv_figure/')
 # year,astronomy_and_astrophysics biology computer_science condensed_matter finance high-energy_physics mathematics other_physics statistics TOTAL
 yearly <- read.csv('submissions_by_area_by_year_to_2011.dat', header = F)
 yearly <- yearly[, -ncol(yearly)]
@@ -27,4 +27,4 @@ pcolor[9] <- "#d53d4e"
 
 figure_2 <-  ggplot(cleaned_df[complete.cases(cleaned_df),], aes(year, value, group = variable,color = variable)) + geom_line() + geom_line(aes(linetype = shape), size = 1, show_guide = FALSE) + expand_limits(0,0) + labs(y="Cumulative submissions", x="Year") + ggtitle("arXiv submissions by discipline") + guides(colour = guide_legend(title = "Disciplines", title.hjust = 0.5, keywidth = 1, keyheight = 1, override.aes = list(size = 4))) + scale_color_manual(values = pcolor,  name = "Discipline") + theme_gray()
 
-ggsave(figure_2, file = "figure_2.png", width = 6, height = 4)
+ggsave(figure_2, file = "figure_2.eps", width = 6, height = 4)
